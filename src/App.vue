229ca4bd4b2db3f089db6550539ffd0d70f8f310
patience1000 <paulpatience577@gmail.com>
2023-02-05 22:31:03 +0100

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,116 @@
 <script setup>
 </script>
 
-<template>
-   <body style="background-color: #f2f2f2">
-        <!-- Furniture slide -->
-        <div class="Sale" style="background-image=">
-            <marquee><h2>Buy at affordable prices</h2></marquee>
-            <div class="switch-btn">
-                <button class="sale2-btn" onclick="Show2()">Prev</button>
-                <button class="sale1-btn" onclick="Show1()">Next</button>
-            </div>
-            <div id="div1">
-                <div class="sale1">
-                    <div class="sale1-section">
-                        <img v-for="img in sale1" v-bind:src="img"/>
-                        <h3>Palour Sofa</h3>
-                        <p>Type: yellow sofa</p>
-                        <p>Price: $350</p>
-                        <p>Quantity: <input type="number" placeholder="0"></p>
-                        <div class="line"></div>
-                        <button><a href="#">Add to cart</a></button>
-                    </div> 
-        
-                    <div class="sale1-section">
-                        <img v-for="img in sale2" v-bind:src="img"/>
-                        <h3>White Sofa</h3>
-                        <p>Type: white sofa</p>
-                        <p>Price: $450</p>
-                        <p>Quantity: <input type="number" placeholder="0"></p>
-                        <div class="line"></div>
-                        <button><a href="#">Add to cart</a></button>
-                    </div>
-        
-                    <div class="sale1-section">
-                        <img v-for="img in sale3" v-bind:src="img"/>
-                        <h3>Dinning Set </h3>
-                        <p>Type: yellow sofa</p>
-                        <p>Price: $450</p>
-                        <p>Quantity: <input type="number" placeholder="0"></p>
-                        <div class="line"></div>
-                        <button><a href="#">Add to cart</a></button>
-                    </div>    
-                </div>    
-            </div>
-
-            <div id="div2">
-                <div class="sale2">
-                    <div class="sale2-section">
-                        <img v-for="img in sale4" v-bind:src="img"/>
-                        <h3>Lamp</h3>
-                        <p>Type: yellow sofa</p>
-                        <p>Price: $450</p>
-                        <p>Quantity: <input type="number" placeholder="0"></p>
-                        <div class="line"></div>
-                        <button><a href="#">Add to cart</a></button>
-                    </div> 
-        
-                    <div class="sale2-section">
-                        <img v-for="img in sale5" v-bind:src="img"/>
-                        <h3>Loxy Sofa</h3>
-                        <p>Type: yellow sofa</p>
-                        <p>Price: $450</p>
-                        <p>Quantity: <input type="number" placeholder="0"></p>
-                        <div class="line"></div>
-                        <button><a href="#">Add to cart</a></button>
-                    </div>
-        
-                    <div class="sale2-section">
-                        <img v-for="img in sale6" v-bind:src="img"/>
-                        <h3>Ankara Sofa</h3>    
-                        <p>Type: yellow sofa</p>
-                        <p>Price: $450</p>
-                        <p>Quantity: <input type="number" placeholder="0"></p>
-                        <div class="line"></div>
-                        <button><a href="#">Add to cart</a></button>
-                    </div>
+    <template>
+    <body style="background-color: #f2f2f2">
+            <!-- Furniture slide -->
+            <div class="Sale" style="background-image=">
+                <marquee><h2>Buy at affordable prices</h2></marquee>
+                <div class="switch-btn">
+                    <button class="sale2-btn" onclick="Show2()">Prev</button>
+                    <button class="sale1-btn" onclick="Show1()">Next</button>
                 </div>
-            </div>  
-        </div>
-        <div id="about-section">
-        <div class="abt-txt">
-            <h1>About Us</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum quos dicta,min<br>Da ipsam ut tenetur doloremque suscipit consequatur nobis quis.</p>
-            <div class="point">
-               
-                <div class="points">
-                    <div class="circle">P</div>
-                    <h4>PROFFESIONAL TEAM</h4>
-                </div>
-                <p>Lorem ipsum,dolor sit amet consectetur adipisicing <br> consequatur nobis.</p>
-
-                <div class="points">
-                    <div class="circle">E</div>
-                    <h4>EXPERT ADVICE</h4>
-                </div>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing <br> consequatur nobis.</p>
-                
-                <div class="points">
-                    <div class="circle">C</div>
-                    <h4>CREATIVE IDEAS</h4>
-                </div>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing <br> consequatur nobis.</p>
-
-            </div>
+                <div id="div1">
+                    <div class="sale1">
+                        <div class="sale1-section">
+                            <img v-for="img in sale1" v-bind:src="img"/>
+                            <h3>Palour Sofa</h3>
+                            <p>Type: yellow sofa</p>
+                            <p>Price: $350</p>
+                            <p>Quantity: <input type="number" placeholder="0"></p>
+                            <div class="line"></div>
+                            <button><a href="#">Add to cart</a></button>
+                        </div> 
             
-        </div>
-        <div class="abt-img">
-            <img v-for="img in images" v-bind:src="img"/>
+                        <div class="sale1-section">
+                            <img v-for="img in sale2" v-bind:src="img"/>
+                            <h3>White Sofa</h3>
+                            <p>Type: white sofa</p>
+                            <p>Price: $450</p>
+                            <p>Quantity: <input type="number" placeholder="0"></p>
+                            <div class="line"></div>
+                            <button><a href="#">Add to cart</a></button>
+                        </div>
+            
+                        <div class="sale1-section">
+                            <img v-for="img in sale3" v-bind:src="img"/>
+                            <h3>Dinning Set </h3>
+                            <p>Type: yellow sofa</p>
+                            <p>Price: $450</p>
+                            <p>Quantity: <input type="number" placeholder="0"></p>
+                            <div class="line"></div>
+                            <button><a href="#">Add to cart</a></button>
+                        </div>    
+                    </div>    
+                </div>
+
+                <div id="div2">
+                    <div class="sale2">
+                        <div class="sale2-section">
+                            <img v-for="img in sale4" v-bind:src="img"/>
+                            <h3>Lamp</h3>
+                            <p>Type: yellow sofa</p>
+                            <p>Price: $450</p>
+                            <p>Quantity: <input type="number" placeholder="0"></p>
+                            <div class="line"></div>
+                            <button><a href="#">Add to cart</a></button>
+                        </div> 
+            
+                        <div class="sale2-section">
+                            <img v-for="img in sale5" v-bind:src="img"/>
+                            <h3>Loxy Sofa</h3>
+                            <p>Type: yellow sofa</p>
+                            <p>Price: $450</p>
+                            <p>Quantity: <input type="number" placeholder="0"></p>
+                            <div class="line"></div>
+                            <button><a href="#">Add to cart</a></button>
+                        </div>
+            
+                        <div class="sale2-section">
+                            <img v-for="img in sale6" v-bind:src="img"/>
+                            <h3>Ankara Sofa</h3>    
+                            <p>Type: yellow sofa</p>
+                            <p>Price: $450</p>
+                            <p>Quantity: <input type="number" placeholder="0"></p>
+                            <div class="line"></div>
+                            <button><a href="#">Add to cart</a></button>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+            <div id="about-section">
+            <div class="abt-txt">
+                <h1>About Us</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum quos dicta,min<br>Da ipsam ut tenetur doloremque suscipit consequatur nobis quis.</p>
+                <div class="point">
+                
+                    <div class="points">
+                        <div class="circle">P</div>
+                        <h4>PROFFESIONAL TEAM</h4>
+                    </div>
+                    <p>Lorem ipsum,dolor sit amet consectetur adipisicing <br> consequatur nobis.</p>
+
+                    <div class="points">
+                        <div class="circle">E</div>
+                        <h4>EXPERT ADVICE</h4>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing <br> consequatur nobis.</p>
+                    
+                    <div class="points">
+                        <div class="circle">C</div>
+                        <h4>CREATIVE IDEAS</h4>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing <br> consequatur nobis.</p>
+
+                </div>
+                
+            </div>
+            <div class="abt-img">
+                <img v-for="img in images" v-bind:src="img"/>
+            </div>          
         </div>          
-      </div>          
-   </body>
-</template>
+    </body>
+    </template>
   
 <script>
     export default {
